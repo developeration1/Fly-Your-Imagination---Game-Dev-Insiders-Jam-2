e5a26c1bd052b782e5b9c96e62fadca5ad2a34a9
Adrián Rangel Suárez <arangelsuarez@hotmail.com>
2020-12-04 21:40:43 -0600

    Functional UFO
    UFO (OVNI) totally functional

---
+++ Fly Your Imagination/Assets/OVNI/OvniController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OvniController : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 5;
+
+    Rigidbody2D _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+
+        _rb.MovePosition(_rb.position + Vector2.right * x * speed * Time.fixedDeltaTime);
+    }
+}
---
+++ Fly Your Imagination/Assets/OVNI/OvniMagnet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OvniMagnet : MonoBehaviour
+{
+    [SerializeField]
+    float magnetForce = .5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Cow"))
+        {
+            collision.attachedRigidbody.MovePosition(Vector2.MoveTowards(collision.attachedRigidbody.position, transform.parent.position, magnetForce * Time.deltaTime));
+        }
+    }
+}
